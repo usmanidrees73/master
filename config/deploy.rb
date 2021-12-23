0c0b00a696a6e3e9c698c9bf195032cfaa527b18
usmanidrees73 <uiusmanidrees481@gmail.com>
2021-12-23 18:28:00 +0500

    deployment stuff

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,20 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.16.0"
 
-set :application, "rails-chat-tutorial"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "rails-chat-app"
+set :repo_url, "git@github.com:usmanidrees73/master.git"
+
+# Deploy to the user's home directory
+set :deploy_to, "/home/deploy/#{fetch :application}"
+
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+
+# Only keep the last 5 releases to save disk space
+set :keep_releases, 5
+
+# Optionally, you can symlink your database.yml and/or secrets.yml file from the shared directory during deploy
+# This is useful if you don't want to use ENV variables
+# append :linked_files, 'config/database.yml', 'config/secrets.yml'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,6 +8,8 @@
 # server "db.example.com", user: "deploy", roles: %w{db}
 
 
+server '161.35.255.173', user: 'deploy', roles: %w{app db web}
+
 
 # role-based syntax
 # ==================
