9cd4b914a506d3c2bfd951637155f1cc4c87773f
usmanidrees73 <uiusmanidrees481@gmail.com>
2021-12-23 17:13:29 +0500

    chat app

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,59 @@
+class RoomsController < ApplicationController
+  # Loads:
+  # @rooms = all rooms
+  # @room = current room when applicable
+  before_action :load_entities
+
+  def index
+    debugger
+    @rooms = Room.all
+  end
+
+  def new
+    debugger
+    @room = Room.new
+  end
+
+  def create
+    debugger
+    @room = Room.new permitted_parameters
+
+    if @room.save
+      flash[:success] = "Room #{@room.name} was created successfully"
+      redirect_to rooms_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    debugger
+    @room_message = RoomMessage.new room: @room
+    @room_messages = @room.room_messages.includes(:user)
+  end
+
+  def edit
+    debugger
+  end
+
+  def update
+    debugger
+    if @room.update_attributes(permitted_parameters)
+      flash[:success] = "Room #{@room.name} was updated successfully"
+      redirect_to rooms_path
+    else
+      render :new
+    end
+  end
+
+  protected
+
+  def load_entities
+    @rooms = Room.all
+    @room = Room.find(params[:id]) if params[:id]
+  end
+
+  def permitted_parameters
+    params.require(:room).permit(:name)
+  end
+end
